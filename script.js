f00ccdf8cdbf8f1a494d82b7272ba30231c55257
Doğa Kayra Yılmazarslan <94742646+Noxtrah@users.noreply.github.com>
2024-03-19 18:24:59 +0300

    Started to work on selectors in flight tab

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+window.onload = function() {
+    const initialButton = document.querySelector('.booker-button.active');
+    handleButtonClick(initialButton);
+};
+
 async function handleButtonClick(button) {
     // Remove active class from all buttons
     const buttons = document.querySelectorAll('.booker-button');
     buttons.forEach(btn => {
         btn.classList.remove('active');
     });
-    // Add active class to the parent <li> element of the clicked button
+
     button.classList.add('active');
-    
-    // Check if the active button has a specific ID
+
     const buttonId = button.id;
     console.log("Active button ID: ", buttonId);
 
-    if (buttonId === 'booker-button1') { // Check if the first button is clicked
+    if (buttonId === 'booker-button1') {
         console.log("First button is active");
         await handleFlightButton();
     } else if(buttonId === 'booker-button2'){
@@ -26,8 +30,9 @@ function handleFlightButton(){
     return new Promise((resolve, reject) => {
         const tabContent = document.querySelector('.tab-content');
         tabContent.innerHTML = '';
-        // Create and append radio buttons
+
         const roundTrip = createRadioButton('Round Trip', 'RoundTrip');
+        roundTrip.classList.add('active'); //Make this radio button active initially
         const roundTripLabel = createLabel('Round Trip', 'RoundTrip');
         tabContent.appendChild(roundTrip);
         tabContent.appendChild(roundTripLabel);
@@ -47,6 +52,58 @@ function handleFlightButton(){
         tabContent.appendChild(multiCity);
         tabContent.appendChild(multiCityLabel);
 
+        const awardTicket = createDiv('award-ticket-div');
+        const spanElement = document.createElement('span');
+        spanElement.classList.add('award-ticket-span');
+        spanElement.textContent = 'Award ticket - Buy a ticket with Miles';
+
+        const emElement = document.createElement('em');
+
+        const imgElement = document.createElement('img');
+        imgElement.classList.add('award-ticket-icon');
+        imgElement.src = 'https://www.turkishairlines.com/assets/ms-log-in-aaf383a1.svg';
+
+        emElement.appendChild(imgElement);
+
+        awardTicket.appendChild(emElement);
+        awardTicket.appendChild(spanElement);
+        tabContent.appendChild(awardTicket);
+
+        const originSelector = createDiv('origin-selector');
+
+        const originLabel = createLabel('From');
+        originLabel.classList.add('origin-label');
+        originSelector.appendChild(originLabel);
+
+        const portWrapper = createDiv('port-wrapper');
+        originSelector.appendChild(portWrapper);
+
+        const originInput = document.createElement('input');
+        originInput.classList.add('origin-input');
+        originInput.value = 'Izmir';
+        const originInputValue = document.createElement('span');
+        originInputValue.classList.add('origin-input-value');
+        originInputValue.textContent = originInput.value;
+
+        portWrapper.appendChild(originInputValue);
+
+        const originLabel2 = createLabel('Izmir Adnan ... (ADB)');
+        originLabel2.classList.add('origin-label2');
+        originSelector.appendChild(originLabel2);
+
+        const changePorts = document.createElement('i');
+        changePorts.classList.add('change-ports-icon');
+        originSelector.appendChild(changePorts);
+
+        const destinationSelector = createDiv('destination-selector');
+        originSelector.appendChild(destinationSelector);
+
+        const destinationLabel = createLabel('To');
+        destinationLabel.classList.add('destination-label');
+        destinationSelector.appendChild(destinationLabel);
+
+        tabContent.appendChild(originSelector);
+
         resolve();
     });
 }
@@ -57,7 +114,7 @@ function createRadioButton(labelText, name) {
     radioButton.href = '#'; // To make it behave like a link
     radioButton.dataset.name = name; // Using dataset to store the name
     radioButton.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent the default link behavior
+        event.preventDefault();
         const allRadioButtons = document.querySelectorAll('.flight-radio-buttons');
         allRadioButtons.forEach(btn => {
             btn.classList.remove('active');
@@ -73,6 +130,12 @@ function createLabel(labelText, htmlFor) {
     label.htmlFor = htmlFor;
     label.textContent = labelText;
     return label;
+}
+
+function createDiv(classToBeAdded){
+    const div = document.createElement('div');
+    div.classList.add(classToBeAdded);
+    return div;
 }
 
 function handleCheckinAndManageBookingButton(){
